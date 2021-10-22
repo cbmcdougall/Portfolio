a8ad287cc1c7301d103f1daf0ad197e238f8031e
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-22 19:55:18 +0100

    slight cleanup

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,21 @@
-// Determine current active button, default to home
 window.onload = () => {
+    // Determine current active button, default to home
     const currentSection = window.location.hash.split('#')[1];
     const idSelector = currentSection || "home"
+    // Toggle on button as active
     const sectionButton = document.getElementById(`${idSelector}-button`);
     sectionButton.classList.toggle("isactive");
 }
 
 
 // Toggle active button on click
-const buttons = document.querySelectorAll(".navbutton");
+const buttons = document.querySelectorAll(".nav-button");
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        // If another button is active, toggle it off
+        // Toggle off currently active button
         const activeButton = document.querySelector(".isactive");
         activeButton.classList.toggle("isactive");
+        // Toggle on clicked button as active
         button.classList.toggle("isactive");
     })
 })
