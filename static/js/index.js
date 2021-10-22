d93e083d970444d8c33b86d76667bc246ab93518
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-22 12:52:10 +0100

    style navbar, home section

---
+++ static/js/index.js
@@ -0,0 +1,18 @@
+// Determine current active button, default to home
+window.onload = () => {
+    const currentSection = window.location.hash.split('#')[1];
+    const sectionButton = document.getElementById(currentSection || "home");
+    sectionButton.classList.toggle("isactive");
+}
+
+
+// Toggle active button on click
+const buttons = document.querySelectorAll(".navbutton");
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        // If another button is active, toggle it off
+        const activeButton = document.querySelector(".isactive");
+        activeButton.classList.toggle("isactive");
+        button.classList.toggle("isactive");
+    })
+})
