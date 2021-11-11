8745efad111990900a028807da69aa9119f1731d
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-11 18:59:54 +0000

    add some styling

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -12,21 +12,21 @@ export const Contact = () => {
             <div className="contact-container">
                 <div id="contactMethods">
                     <div className="contact-method">
-                        <FontAwesomeIcon className="contact-icon" icon={faEnvelope} size="2x" />
+                        <FontAwesomeIcon className="contact-icon" icon={faEnvelope} size="3x" />
                         <div className="contact-method-text">
                             <h3 className="contact-method-title">Email</h3>
                             <span className="contact-method-subtitle">c_mcdougall97@yahoo.co.uk</span>
                         </div>
                     </div>
                     <div className="contact-method">
-                        <FontAwesomeIcon className="contact-icon" icon={faGithubSquare} size="2x" />
+                        <FontAwesomeIcon className="contact-icon" icon={faGithubSquare} size="3x" />
                         <div className="contact-method-text">
                             <h3 className="contact-method-title">Github</h3>
                             <span className="contact-method-subtitle">cbmcdougall</span>
                         </div>
                     </div>
                     <div className="contact-method">
-                        <FontAwesomeIcon className="contact-icon" icon={faMapMarkerAlt} size="2x" />
+                        <FontAwesomeIcon className="contact-icon" icon={faMapMarkerAlt} size="3x" />
                         <div className="contact-method-text">
                             <h3 className="contact-method-title">Location</h3>
                             <span className="contact-method-subtitle">Fife</span>
