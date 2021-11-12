aaee5056926d8649249b49a3989b6270d6de9f1e
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-12 12:46:55 +0000

    add styling

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -34,9 +34,9 @@ export const Contact = () => {
                             </div>
                         </div>
                     </div>
-                    {/* <ContactForm /> */}
                 </div>
             </section>
+            {/* <ContactForm /> */}
         </main>
     )
 }
