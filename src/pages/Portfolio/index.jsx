75c4d0ba62c9536acd0f23645418cc8b7cba018d
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-12 14:50:08 +0000

    add portfolio page styling

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -12,11 +12,15 @@ export const Portfolio = () => {
     return (
         <main id="portfolio">
             <section id="technologies">
-                <h2 className="portfolio-section-header">Known Technologies</h2>
+                <div className="portfolio-section-heading">
+                    <h2 className="portfolio-section-header">Known Technologies</h2>
+                </div>
                 <div className="portfolio-flex">{renderTechnologies()}</div>
             </section>
             <section id="projects">
-                <h2 className="portfolio-section-header">Projects</h2>
+                <div className="portfolio-section-heading">
+                    <h2 className="portfolio-section-header">Projects</h2>
+                </div>
                 <div className="portfolio-flex">{renderProjects()}</div>
             </section>
         </main>
